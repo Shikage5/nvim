048bb0be7c89827f3dbef8f3a232bca4c447086c
Gabriel Salajeanu <gabriel.salajeanu@bechtle.com>
2025-07-30 15:14:16 +0200

    refactor: cleanup lua files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 local builtin = require('telescope.builtin')
 
 return {
-	'nvim-telescope/telescope.nvim', 
+	'nvim-telescope/telescope.nvim',
 	tag = '0.1.8',
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	keys = {
--- lua/shikage/init.lua
+++ lua/shikage/init.lua
@@ -6,15 +6,3 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.scrolloff = 5
-
-vim.g.clipboard = {
-	name  = 'clip.exe',
-	copy  = {
-		['+'] = 'clip.exe',
-		['*'] = 'clip.exe',
-	},
-	paste = {
-		['+'] = 'powershell.exe -c Get-Clipboard',
-		['*'] = 'powershell.exe -c Get-Clipboard',
-	},
-}
