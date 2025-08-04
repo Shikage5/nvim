e1adab1cfb25cc1e61182cd500b30ad9108ec70c
Gabriel Salajeanu <gabriel.salajeanu@bechtle.com>
2025-08-04 16:05:04 +0200

    feat(general): add tab as spaces for intellij, add shortcut for navigating quickfix list

--- lua/shikage/init.lua
+++ lua/shikage/init.lua
@@ -8,29 +8,33 @@ vim.o.shiftwidth = 4
 vim.o.scrolloff = 5
 vim.o.signcolumn = "yes"
 vim.o.wrap = false
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.autoindent = true
+
 
 -- Highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
-  pattern = "*",
-  callback = function()
-    vim.highlight.on_yank {
-      higroup = "IncSearch", -- You can change this to any highlight group
-      timeout = 200,     -- Time in milliseconds
-    }
-  end,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank {
+			higroup = "IncSearch", -- You can change this to any highlight group
+			timeout = 200,     -- Time in milliseconds
+		}
+	end,
 })
 
 --Clipboard for Windows with WSL
 vim.g.clipboard = {
-  name = 'WslClipboard',
-  copy = {
-    ['+'] = 'clip.exe',
-    ['*'] = 'clip.exe',
-  },
-  paste = {
-    ['+'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write(($([Console]::In.ReadToEnd()) -replace "`r", ""))',
-    ['*'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write(($([Console]::In.ReadToEnd()) -replace "`r", ""))',
-  },
-  cache_enabled = 0,
+	name = 'WslClipboard',
+	copy = {
+		['+'] = 'clip.exe',
+		['*'] = 'clip.exe',
+	},
+	paste = {
+		['+'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write(($([Console]::In.ReadToEnd()) -replace "`r", ""))',
+		['*'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write(($([Console]::In.ReadToEnd()) -replace "`r", ""))',
+	},
+	cache_enabled = 0,
 }
 
--- lua/shikage/remap.lua
+++ lua/shikage/remap.lua
@@ -2,3 +2,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>z", vim.cmd.ZenMode)
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
+vim.keymap.set("n", "<C-j>", vim.cmd.cn)
+vim.keymap.set("n", "<C-k>", vim.cmd.cp)
+
